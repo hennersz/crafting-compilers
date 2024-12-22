ac1703ac458dabb277996911dfcea8057425b2ec
Henry Mortimer <henry@morti.net>
2024-12-22 21:43:05 +0000

    build: fudge version on prs

--- package.nix
+++ package.nix
@@ -2,11 +2,13 @@
   lib,
   version,
   buildGradleApplication,
+  pkgs
 }:
 buildGradleApplication {
   pname = "klox";
   version = version;
   src = ./klox;
+  nativeBuildInputs = [pkgs.libgcc];
   meta = with lib; {
     description = "Hello World Application";
     longDescription = ''
